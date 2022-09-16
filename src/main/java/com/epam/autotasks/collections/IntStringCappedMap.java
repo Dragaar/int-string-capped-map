8feec58c9f3f1b3e64738aa2b6fa10d97c82cc3e
Dragaar <rosivanyshyn@gmail.com>
2022-09-16 14:07:21 +0300

    111

--- src/main/java/com/epam/autotasks/collections/IntStringCappedMap.java
+++ src/main/java/com/epam/autotasks/collections/IntStringCappedMap.java
@@ -1,17 +1,15 @@
 package com.epam.autotasks.collections;
 
-import java.util.AbstractMap;
-import java.util.AbstractSet;
-import java.util.ArrayDeque;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
-import java.util.Queue;
-import java.util.Set;
+import java.util.*;
 
 class IntStringCappedMap extends AbstractMap<Integer, String> {
 
     private final long capacity;
+    private long currentCapacity;
+
+
+    LinkedHashMap<Integer, String> map = new LinkedHashMap<>();
+    //SimpleEntry<Integer, String>[] entries;
 
     public IntStringCappedMap(final long capacity) {
         this.capacity = capacity;
@@ -23,53 +21,69 @@ class IntStringCappedMap extends AbstractMap<Integer, String> {
 
     @Override
     public Set<Entry<Integer, String>> entrySet() {
-        return new AbstractSet<>() {
-            @Override
-            public Iterator<Entry<Integer, String>> iterator() {
-                return new Iterator<>() {
-                    @Override
-                    public boolean hasNext() {
-                        //implement this method
-                        throw new UnsupportedOperationException();
-                    }
-
-                    @Override
-                    public Entry<Integer, String> next() {
-                        //implement this method
-                        throw new UnsupportedOperationException();
-                    }
-                };
-            }
-
-            @Override
-            public int size() {
-                return IntStringCappedMap.this.size();
-            }
-        };
+        return map.entrySet();
     }
 
     @Override
     public String get(final Object key) {
-        //implement this method
-        throw new UnsupportedOperationException();
+        return map.get(key);
     }
 
     @Override
     public String put(final Integer key, final String value) {
-        //implement this method
-        throw new UnsupportedOperationException();
+        if(value.length() > capacity) throw new IllegalArgumentException();
+
+        //якщо вже існує дублікат, то замінити його значення на нове
+        if(map.containsKey(key)) {
+            //System.out.println("dubl = " + key + " / " + value);
+
+            currentCapacity -= map.get(key).length();
+            map.remove(key);
+
+            map.put(key, value);
+            currentCapacity += value.length();
+
+            ifCapacityOutOfRange();
+            return key.toString();
+        }
+
+
+        String putObj = map.put(key, value);
+        currentCapacity += value.length();
+
+        ifCapacityOutOfRange();
+
+        //System.out.println("map = " + map);
+        return putObj;
+
     }
 
+    private void ifCapacityOutOfRange (){
+        //якщо результуючий розмір перевищує capacity
+        while(currentCapacity > capacity)
+        {
+            Iterator<Entry<Integer, String>> iterator = map.entrySet().iterator();
+            if(iterator.hasNext())
+            {
+                Entry<Integer, String> value = iterator.next();
+                //System.out.println("removeFirst = " + value.getKey() +" / " + currentCapacity + " / " + capacity);
+                //iterator.next();
+                currentCapacity -= value.getValue().length();
+                iterator.remove();
+            } else break;
+        }
+    }
     @Override
     public String remove(final Object key) {
-        //implement this method
-        throw new UnsupportedOperationException();
+        String remuveObj = map.remove(key);
+
+        if(remuveObj != null) currentCapacity -= remuveObj.length();
+        return remuveObj;
     }
 
     @Override
     public int size() {
-        //implement this method
-        throw new UnsupportedOperationException();
+        return map.size();
     }
 
 }
